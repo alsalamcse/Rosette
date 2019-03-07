9e0b129c756f42bbe2e08ad111f4a7db62c2c733
Owner <alsalamcse>
2019-03-07 14:52:33 +0200

    Adapter: btnEdit

--- app/src/main/java/draz/rosette/rosettesms/data/Adapter.java
+++ app/src/main/java/draz/rosette/rosettesms/data/Adapter.java
@@ -2,6 +2,7 @@ package draz.rosette.rosettesms.data;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import draz.rosette.rosettesms.MessageActivity;
 import draz.rosette.rosettesms.R;
 
 public class Adapter extends ArrayAdapter <MyMessage> {
@@ -20,10 +22,9 @@ public class Adapter extends ArrayAdapter <MyMessage> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
 
-            //    convertView=LayoutInflater.from(getContext()).inflate(R)
-            MyMessage myMessage = getItem(position);
+        if (convertView == null) {
+            final MyMessage myMessage = getItem(position);
             TextView tvto = convertView.findViewById(R.id.tvto);
             TextView tvReciver = convertView.findViewById(R.id.tvReciver);
             TextView tvTimePicked = convertView.findViewById(R.id.tvTimePicked);
@@ -32,12 +33,22 @@ public class Adapter extends ArrayAdapter <MyMessage> {
             Button ibtnDelete = convertView.findViewById(R.id.ibtnDelete);
 
 
+
+
+
             tvto.setText(myMessage.getToTitle());
             tvTime.setText(myMessage.getOnTitle());
             tvTimePicked.setText(myMessage.getTime());
             tvReciver.setText(myMessage.getReciver());
             tvTimePicked.setText(myMessage.getTime());
-           // ibtnEdit
+           ibtnEdit.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                 Intent i=new Intent(getContext(), MessageActivity.class);
+                i.putExtra("message", (Parcelable) myMessage);
+                   getContext().startActivity(i);
+               }
+           });
 
 
 
